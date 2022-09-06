7a5ad2cefb00aade9ec32226d9d55713b97fa9ae
Bishnu <bishnuthapako@gmail.com>
2022-09-06 15:13:43 -0400

    completed

--- app/controllers/members_only_articles_controller.rb
+++ app/controllers/members_only_articles_controller.rb
@@ -1,5 +1,6 @@
 class MembersOnlyArticlesController < ApplicationController
   rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
+  before_action :authorize
 
   def index
     articles = Article.where(is_member_only: true).includes(:user).order(created_at: :desc)
@@ -7,11 +8,23 @@ class MembersOnlyArticlesController < ApplicationController
   end
 
   def show
-    article = Article.find(params[:id])
-    render json: article
+
+    session[:page_views] ||= 0
+    session[:page_views] += 1
+
+    if session[:page_views] <=50
+      article = Article.find(params[:id])
+      render json: article
+    else
+      render json: {error: "Maximum pageview limit reached"}, status: :unauthorized
+    end
   end
 
   private
+
+    def authorize
+      return render json: { error: "Not authorized" }, status: :unauthorized unless session.include? :user_id
+    end
 
   def record_not_found
     render json: { error: "Article not found" }, status: :not_found
